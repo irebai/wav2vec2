ec01d74759537d9f1d7af1e0f7b711dbe931cc30
ilyes rebai <irebai@linagora.com>
2021-06-04 14:22:03 +0000

    update file evaluation

--- eval_file.py
+++ eval_file.py
@@ -2,34 +2,57 @@
 from transformers import (
     Wav2Vec2CTCTokenizer,
     Wav2Vec2FeatureExtractor,
-    Wav2Vec2Processor,
-    Wav2Vec2ForCTC
+    Wav2Vec2Processor
 )
+from module.model import Wav2Vec2ForCTC
+
 import torch
 import torchaudio
+import os
+import argparse
+
+def decode(
+    model_dir,
+    file_path
+):
+    processor = Wav2Vec2Processor.from_pretrained(model_dir)
+    model = Wav2Vec2ForCTC.from_pretrained(model_dir)
+
+    speech_array, sampling_rate = torchaudio.load(file_path)
+    if sampling_rate != 16000:
+        resampler = torchaudio.transforms.Resample(sampling_rate, 16_000)
+        speech_array = resampler(speech_array)
+
+    speech_array = speech_array.squeeze().numpy()
+    inputs = torch.tensor([speech_array])
+
+    with torch.no_grad():
+        logits = model(inputs).logits
+
+    predicted_ids = torch.argmax(logits, dim=-1)
+    predicted_sentence = processor.batch_decode(predicted_ids)
+
+    print(predicted_sentence)
 
 
-model_dir="/workspace/huggingface_models/wav2vec2-large-xlsr-53-french"
-file_path="/workspace/wavs/Abdel_validation_lvcsr.wav"
-file_path="/workspace/wavs/hublot_2019_gafa.wav"
-#file_path="/workspace/wavs/1.wav"
-#file_path="/workspace/wavs/5.wav"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--model',
+        help='wav2vec2 model path',
+        type=str,
+        required=True)
+    parser.add_argument(
+        '--file',
+        help='tokenizer type used for training the model',
+        type=str,
+        required=True)
+    eval_args = parser.parse_args()
 
-processor = Wav2Vec2Processor.from_pretrained(model_dir)
-model = Wav2Vec2ForCTC.from_pretrained(model_dir)
+    if not os.path.exists(eval_args.file):
+        raise ValueError("The provided file "+eval_args.file+" doesn't exist")
 
-speech_array, sampling_rate = torchaudio.load(file_path)
-if sampling_rate != 16000:
-    resampler = torchaudio.transforms.Resample(sampling_rate, 16_000)
-    speech_array = resampler(speech_array)
-
-speech_array = speech_array.squeeze().numpy()
-inputs = torch.tensor([speech_array])
-
-with torch.no_grad():
-    logits = model(inputs).logits
-
-predicted_ids = torch.argmax(logits, dim=-1)
-predicted_sentence = processor.batch_decode(predicted_ids)
-
-print(predicted_sentence)
+    decode(
+        model_dir=eval_args.model,
+        file_path=eval_args.file
+    )
